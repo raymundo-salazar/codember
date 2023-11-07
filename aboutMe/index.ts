d9758a0d25c4bc84d0a407aa6af09c866158575a
rsalazar <asalazar@forzatrans.com>
2023-11-07 10:22:55 -0600

    add about me command to cli

---
+++ aboutMe/index.ts
@@ -0,0 +1,27 @@
+export const es = `
+¡Hola! Soy Raymundo Salazar, originario de Nuevo León, México. Con aproximadamente 10 años de experiencia en el desarrollo web, mi enfoque principal ha sido el de Front End Developer, aunque en varias ocasiones he tenido que desempeñarme como FullStack. Mi camino en la tecnología comenzó de manera inusual.
+
+Inicié mi carrera estudiando Diseño Gráfico con énfasis en medios digitales y audiovisuales. Trabajé como editor de video y 3D en una casa productora audiovisual. Sin embargo, mi historia dio un giro cuando me uní a una agencia de publicidad como director de escena y postproducción. Aquí, surgió la necesidad de contratar a un desarrollador web, ya que las páginas web de nuestros clientes se creaban en Flash Macromedia en un momento en que los teléfonos inteligentes estaban en pleno auge y no soportaban Flash. Esta transición a HTML se volvió imperativa.
+
+Al buscar candidatos y no encontrar ninguno, me vi en la necesidad de aprender las bases del desarrollo web para ayudar al equipo a satisfacer las necesidades de los clientes. Pronto, me vi trabajando en mi primer gran proyecto: un e-commerce desarrollado en PHP. Me enamoré de la programación web y desde entonces no he dejado de aprender y crecer.
+
+Además de mi experiencia en desarrollo web, tengo un título como Experto en Experiencia de Usuario (UX). He trabajado como consultor UX para el gobierno de mi estado y he participado en el desarrollo de varios proyectos exitosos, incluyendo el sitio web de nuestro negocio familiar, diloconflores.com, y una próxima aplicación móvil. He creado cinco aplicaciones móviles que actualmente están en el mercado, y algunas más que, por diversas razones, aún no han visto la luz o se han retirado.
+
+Mi pasión por la tecnología y el diseño me ha llevado a convertirme en un apasionado desarrollador web y experto en experiencia de usuario. Estoy emocionado por lo que el futuro tiene reservado en este emocionante mundo digital.
+
+Siempre estoy dispuesto a conectar y colaborar con colegas y entusiastas del mundo digital. Si deseas ponerte en contacto conmigo, ya sea para discutir un proyecto potencial, compartir conocimientos o simplemente tener una charla amigable sobre desarrollo web y diseño, no dudes en escribirme a raymundo.salser@hotmail.com. También puedes conectarte conmigo en LinkedIn (https://linkedin.com/in/raymundosalazar). ¡Espero con interés recibir tus mensajes y explorar nuevas oportunidades!
+`
+
+export const en = `
+Hello! I'm Raymundo Salazar, originally from Nuevo León, Mexico. With approximately 10 years of experience in web development, my main focus has been as a Front End Developer, although I've had to act as a FullStack Developer on several occasions. My journey into technology began in an unusual way.
+
+I started my career by studying Graphic Design with an emphasis on digital and audiovisual media. I worked as a video and 3D editor in an audiovisual production house. However, my story took a turn when I joined an advertising agency as a director of scenes and post-production. Here, the need for a web developer emerged, as our clients' websites were created in Macromedia Flash at a time when smartphones were on the rise and didn't support Flash. The transition to HTML became imperative.
+
+Unable to find suitable candidates, I found myself needing to learn the basics of web development to assist the team in meeting client needs. Before long, I was working on my first major project, an e-commerce site developed in PHP. I fell in love with web development, and I haven't stopped learning and growing since.
+
+In addition to my web development experience, I hold a title as a User Experience Expert (UX). I've worked as a UX consultant for my state's government and have been involved in various successful projects, including our family business website, diloconflores.com, and an upcoming mobile application. I've developed five mobile applications that are currently in the market and several others that, for various reasons, haven't seen the light of day or have been retired.
+
+My passion for technology and design has led me to become a dedicated web developer and UX expert. I'm excited about what the future holds in this exciting digital world.
+
+I'm always open to connecting and collaborating with fellow professionals and enthusiasts. If you'd like to get in touch, whether it's to discuss a potential project, share insights, or just have a friendly chat about web development and design, feel free to reach out to me at raymundo.salser@hotmail.com. You can also connect with me on LinkedIn (https://linkedin.com/in/raymundosalazar). I'm looking forward to hearing from you and exploring new opportunities!
+`
